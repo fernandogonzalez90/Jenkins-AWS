51138e99a39897dbb4b02ea3854ef2211b8bc038
fernando <fer.ema.gonzalez@gmail.com>
2024-05-23 11:36:21 -0300

    Instala y ejecuta Jenkins

---
+++ Infraestructura/Jenkins/main.tf
@@ -0,0 +1,83 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "jenkins_server" {
+  ami           = "ami-04b70fa74e45c3917"
+  instance_type = "t2.micro"
+  key_name      = "jenkins_key"
+
+  security_groups = [aws_security_group.jenkins_sg.name]
+
+  tags = {
+    Name = "JenkinsServer"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      # Add Docker's official GPG key:
+      "sudo apt-get update",
+      "sudo apt install -y git",
+      "sudo apt-get install -y ca-certificates curl",
+      "sudo install -y -m 0755 -d /etc/apt/keyrings",
+      "sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc",
+      "sudo chmod a+r /etc/apt/keyrings/docker.asc",
+      # Add the repository to Apt sources:
+      "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo \\\"$VERSION_CODENAME\\\") stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+      "sudo apt update",
+      # Install the Docker packages
+      "sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
+      # Run!
+      "sudo docker run -d -p 8080:8080 -p 50000:50000 --name jenkins jenkins/jenkins:lts"
+      # Unblock Jenkins
+      
+    ]
+  }
+
+  provisioner "local-exec" {
+    command = "echo Jenkins URL: http://${aws_instance.jenkins_server.public_ip}:8080"
+  }
+}
+
+resource "aws_security_group" "jenkins_sg" {
+  name_prefix = "jenkins-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 50000
+    to_port     = 50000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "jenkins_key"
+  public_key = file("clave.pub")
+}
+
+output "jenkins_url" {
+  value = "http://${aws_instance.jenkins_server.public_ip}:8080"
+}
+
+
