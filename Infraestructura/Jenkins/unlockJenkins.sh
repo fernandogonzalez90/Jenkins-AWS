47325d7c8e599f99be554a0b502f0184ddb33618
fernando <fer.ema.gonzalez@gmail.com>
2024-05-23 12:05:18 -0300

    update

--- Infraestructura/Jenkins/unlockJenkins.sh
+++ Infraestructura/Jenkins/unlockJenkins.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-# Replace with the actual Jenkins URL
+# URL de Jenkins
 JENKINS_URL="http://localhost:8080"
 
-# Replace with the actual username and password
+# Usuario admin
 USERNAME="admin"
-PASSWORD="temporary_password"
 
-# Use curl to perform an authenticated POST request to the unlock endpoint
+# Obtiene la contraseña inicial
+PASSWORD=$(cat /var/jenkins_home/secrets/initialAdminPassword)
+
+# Obtiene el crumb para la protección CSRF
+CRUMB=$(curl -u $USERNAME:$PASSWORD "$JENKINS_URL/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)")
+
+# Extrae el nombre del campo y el valor del crumb
+CRUMB_FIELD=$(echo $CRUMB | cut -d":" -f1)
+CRUMB_VALUE=$(echo $CRUMB | cut -d":" -f2)
+
+# Realiza la solicitud POST para desbloquear Jenkins
 curl -X POST "$JENKINS_URL/jnlp/console" \
-  -d "username=$USERNAME" \
-  -d "password=$PASSWORD" \
+  -u $USERNAME:$PASSWORD \
+  -H "$CRUMB_FIELD:$CRUMB_VALUE" \
   -H "Content-Type: application/x-www-form-urlencoded"
-
-echo "Jenkins unlocked successfully!"
