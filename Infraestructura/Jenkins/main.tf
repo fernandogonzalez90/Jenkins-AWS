ff7608991aa318ad316d8bd08cf6a901e7e484c1
fernando <fer.ema.gonzalez@gmail.com>
2024-05-23 16:26:40 -0300

    update

--- Infraestructura/Jenkins/main.tf
+++ Infraestructura/Jenkins/main.tf
@@ -9,31 +9,36 @@ resource "aws_instance" "jenkins_server" {
 
   security_groups = [aws_security_group.jenkins_sg.name]
 
+  user_data = <<-EOF
+              #!/bin/bash
+              # Actualiza la lista de paquetes
+              sudo apt-get update
+
+              # Instala paquetes necesarios
+              sudo apt-get install -y ca-certificates curl
+
+              # Crea el directorio para las claves GPG de Docker
+              sudo install -y -m 0755 -d /etc/apt/keyrings
+
+              # Descarga la clave GPG oficial de Docker
+              sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+              sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+              # Añade el repositorio de Docker a las fuentes de Apt
+              echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo \"$VERSION_CODENAME\") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+              # Actualiza la lista de paquetes nuevamente
+              sudo apt-get update
+
+              # Instala Docker
+              sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+              # Ejecuta el contenedor de Jenkins
+              sudo docker run -d -p 8080:8080 -p 50000:50000 --name jenkins jenkins/jenkins:lts
+              EOF
+
   tags = {
     Name = "JenkinsServer"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      # Add Docker's official GPG key:
-      "sudo apt-get update",
-      "sudo apt install -y git",
-      "sudo apt-get install -y ca-certificates curl",
-      "sudo install -y -m 0755 -d /etc/apt/keyrings",
-      "sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc",
-      "sudo chmod a+r /etc/apt/keyrings/docker.asc",
-      # Add the repository to Apt sources:
-      "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo \\\"$VERSION_CODENAME\\\") stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
-      "sudo apt update",
-      # Install the Docker packages
-      "sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
-      # Run!
-      "sudo docker run -d -p 8080:8080 -p 50000:50000 --name jenkins jenkins/jenkins:lts"
-    ]
-  }
-
-  provisioner "local-exec" {
-    command = "echo Jenkins URL: http://${aws_instance.jenkins_server.public_ip}:8080"
   }
 }
 
@@ -77,5 +82,3 @@ resource "aws_key_pair" "deployer" {
 output "jenkins_url" {
   value = "http://${aws_instance.jenkins_server.public_ip}:8080"
 }
-
-
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Jenkins en AWS
 Pruebas para desplegar Jenkins en EC2 de AWS con Terraform y Docker.
+
+### En esta practica lo que hago es crear un main.tf que contiene:
+- provider "aws"
+  (que solo contiene la region)
+- resource "aws_instance"
+  (Este contiene la ami, instance_type, key_name, security_groups, un user_data que ejecuta comandos bash para instalar docker y ejecutar una imagen de Jenkins)
+- resource "aws_security_group"
+  (Contiene los ingress y egress correspondientes para conectarse por ssh y acceder a jenkins desde todas las IPs)
+- resource "aws_key_pair"
+  (Este tiene key_name y public_key desde un file)
+- output "jenkins_url"
+  (al finalizar terraform devuelve la ip publica con el puerto para acceder a Jenkins)
+
+<b>Una vez ejecutado todo esto hay que recuperar la clave de Jenkins que se encuentra en :
+"/var/jenkins_home/secrets/initialAdminPassword"
+Esto lo hacemos ingresando a la maquina de EC2 por SSH:</b>
+
+`ssh usuario@ippublica`
+
+Una vez dentro debemos ejecutar una terminal interactiva del contenedor Docker y lo hacemos con:
+
+`sudo docker exec -it jenkins /bin/bash`
+
+Estando dentro del contendor ejecutamos un cat a la ruta que nos dice Jenkins:
+
+`cat /var/jenkins_home/secrets/initialAdminPassword`
+
+Esto nos dara la clave, copiamos y pegamos en la web de Jenkins.
