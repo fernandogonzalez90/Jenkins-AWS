4ee483dd22dc3eaedf4a7f7faab13699f347844a
fernando <fer.ema.gonzalez@gmail.com>
2024-05-31 10:35:06 -0300

    adios

--- Infraestructura/Terraform/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "jenkins_url" {
-  value = "http://${aws_instance.jenkins_server.public_ip}:8080"
-}
--- Infraestructura/Terraform/par_claves.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "deployer" {
-  key_name   = "jenkins_key"
-  public_key = file("clave.pub")
-}
